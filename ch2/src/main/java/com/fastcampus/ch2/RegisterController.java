a88dd6586a85fc310ee1bc0110ebbed628105421
InHyeKyeong <kih9079@naver.com>
2026-04-22 23:07:38 +0900

    22) @InitBinder, @DateTimeFormat

--- ch2/src/main/java/com/fastcampus/ch2/RegisterController.java
+++ ch2/src/main/java/com/fastcampus/ch2/RegisterController.java
@@ -1,13 +1,27 @@
 package com.fastcampus.ch2;
 
 import java.net.URLEncoder;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
+import org.springframework.beans.propertyeditors.CustomDateEditor;
+import org.springframework.beans.propertyeditors.StringArrayPropertyEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class RegisterController {
+
+	@InitBinder
+	public void toDate(WebDataBinder binder) {
+//		SimpleDateFormat df = new SimpleDateFormat("yyyy-mm-dd");
+//		binder.registerCustomEditor(Date.class, new CustomDateEditor(df, false));
+		binder.registerCustomEditor(String[].class, new StringArrayPropertyEditor("#"));
+	}
 
 //	@RequestMapping(value="/register/add", method={RequestMethod.GET, RequestMethod.POST})
 //	@RequestMapping("/register/add")		// 신규회원 가입 화면  
@@ -18,13 +32,16 @@ public class RegisterController {
 
 //	@RequestMapping(value="/register/save", method=RequestMethod.POST)
 	@PostMapping("/register/save") // 4.3부터
-	public String save(User user, Model m) throws Exception {
+	public String save(User user, BindingResult result, Model m) throws Exception {
+
+		System.out.println("result = " + result);
+		System.out.println("user = " + user);
 
 		// 1. 유효성 검사
 
 		if (!isValid(user)) {
-			String msg = URLEncoder.encode("id를 잘못 입력하셨습니다.","utf-8");
-			return "redirect:/register/add?msg=" + msg;		// URL재작성(rewriting)
+			String msg = URLEncoder.encode("id를 잘못 입력하셨습니다.", "utf-8");
+			return "redirect:/register/add?msg=" + msg; // URL재작성(rewriting)
 		}
 		// 2. DB에 신규회원 정보를 저장
 		return "registerInfo";
