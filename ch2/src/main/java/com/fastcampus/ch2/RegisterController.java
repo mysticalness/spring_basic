14841f20875d1983b5c12a7fc0430cdddf092a4d
InHyeKyeong <kih9079@naver.com>
2026-04-22 12:33:49 +0900

    17) 로그인화면 - Cookie

--- ch2/src/main/java/com/fastcampus/ch2/RegisterController.java
+++ ch2/src/main/java/com/fastcampus/ch2/RegisterController.java
@@ -1,19 +1,37 @@
 package com.fastcampus.ch2;
 
+import java.net.URLEncoder;
+
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class RegisterController {
 
-	@RequestMapping("/register/add")
-	public String register() {
-		return "registerForm"; // WEB-INF/views/registerForm.jsp
+//	@RequestMapping(value="/register/add", method={RequestMethod.GET, RequestMethod.POST})
+//	@RequestMapping("/register/add")		// 신규회원 가입 화면  
+//	@GetMapping("/register/add")
+//	public String register() {
+//		return "registerForm"; // WEB-INF/views/registerForm.jsp
+//	}
+
+//	@RequestMapping(value="/register/save", method=RequestMethod.POST)
+	@PostMapping("/register/save") // 4.3부터
+	public String save(User user, Model m) throws Exception {
+
+		// 1. 유효성 검사
+
+		if (!isValid(user)) {
+			String msg = URLEncoder.encode("id를 잘못 입력하셨습니다.","utf-8");
+			return "redirect:/register/add?msg=" + msg;		// URL재작성(rewriting)
+		}
+		// 2. DB에 신규회원 정보를 저장
+		return "registerInfo";
 	}
 
-	@RequestMapping("/register/save")
-	public String save() {
-		return "registerInfo";
+	private boolean isValid(User user) {
+		return true;
 	}
 
 }
--- ch2/src/main/java/com/fastcampus/ch2/WebConfig.java
+++ ch2/src/main/java/com/fastcampus/ch2/WebConfig.java
@@ -2,6 +2,7 @@ package com.fastcampus.ch2;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -10,5 +11,13 @@ public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry.addResourceHandler("/**").addResourceLocations("/resources/");
+	}
+
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+
+		// /register → registerForm.html or registerForm.jsp
+		registry.addViewController("/register/add").setViewName("registerForm"); // GET 요청만 허용
+
 	}
 }
