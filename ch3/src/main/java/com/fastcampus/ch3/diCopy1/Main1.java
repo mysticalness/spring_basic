edc395778815451639fc3e4f52de84c83342a7d2
InHyeKyeong <kih9079@naver.com>
2026-04-25 13:15:03 +0900

    26) Spring DI 1

--- ch3/src/main/java/com/fastcampus/ch3/diCopy1/Main1.java
+++ ch3/src/main/java/com/fastcampus/ch3/diCopy1/Main1.java
@@ -3,14 +3,29 @@ package com.fastcampus.ch3.diCopy1;
 import java.io.FileReader;
 import java.util.Properties;
 
-class Car {}
-class SportsCar extends Car{}
-class Truck extends Car{}
+class Car { }
+
+class SportsCar extends Car { }
+
+class Truck extends Car { }
+
+class Engine { }
 
 public class Main1 {
     public static void main(String[] args) throws Exception {
-        Car car = getCar();
+        Car car = (Car) getObject("car");
+        Engine engine = (Engine) getObject("engine");
         System.out.println("car = " + car);
+        System.out.println("engine = " + engine);
+    }
+
+    static Object getObject(String key) throws Exception {
+        Properties p = new Properties();
+        p.load(new FileReader("config.txt"));
+
+        Class clazz = Class.forName(p.getProperty(key));
+
+        return clazz.newInstance();
     }
 
     static Car getCar() throws Exception {
@@ -19,6 +34,6 @@ public class Main1 {
 
         Class clazz = Class.forName(p.getProperty("car"));
 
-        return (Car)clazz.newInstance();
+        return (Car) clazz.newInstance();
     }
 }
