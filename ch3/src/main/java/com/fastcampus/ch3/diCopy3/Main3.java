2d8775f1b755d6366844c9a9e4a9517311ed0879
InHyeKyeong <kih9079@naver.com>
2026-04-25 14:15:35 +0900

    29) 객체 찾기 - byName, byType

--- ch3/src/main/java/com/fastcampus/ch3/diCopy3/Main3.java
+++ ch3/src/main/java/com/fastcampus/ch3/diCopy3/Main3.java
@@ -59,17 +59,28 @@ class AppContext {
         }
     }
 
-    Object getBean(String key){
+    Object getBean(String key) {  // byName
         return map.get(key);
+    }
+
+    Object getBean(Class clazz) {   // byType
+        for (Object obj : map.values()) {
+            if (clazz.isInstance(obj)) {
+                return obj;
+            }
+        }
+        return null;
     }
 }
 
 public class Main3 {
     public static void main(String[] args) throws Exception {
         AppContext ac = new AppContext();
-        Car car = (Car) ac.getBean("car");
+        Car car = (Car) ac.getBean("car");  // byName
+        Car car2 = (Car) ac.getBean(Car.class);  // byType
         Engine engine = (Engine) ac.getBean("engine");
         System.out.println("car = " + car);
+        System.out.println("car2 = " + car2);
         System.out.println("engine = " + engine);
     }
 
